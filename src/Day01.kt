93a76006126243b735b6f77029ab624897003945
pduong <pduong@streamsystems.ca>
2022-11-30 22:26:44 -0700

    finished day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,35 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val runningTotal = mutableListOf<Int>()
+        var currentTotal = 0
+        input.forEach {
+            if(it.isBlank()) {
+                runningTotal.add(currentTotal)
+                currentTotal = 0
+            } else {
+                currentTotal += it.toInt()
+            }
+        }
+        return runningTotal.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val runningTotal = mutableListOf<Int>()
+        var currentTotal = 0
+        input.forEach {
+            if(it.isBlank()) {
+                runningTotal.add(currentTotal)
+                currentTotal = 0
+            } else {
+                currentTotal += it.toInt()
+            }
+        }
+        runningTotal.sortDescending()
+        return runningTotal[0] + runningTotal[1] + runningTotal[2]
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val testInput = readInput("dayone")
+    println(part1(testInput))
+    println(part2(testInput))
 }
